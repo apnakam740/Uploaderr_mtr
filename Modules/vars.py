ca83ce3e5341d807d3ef7a21f84e589dab07079f
apnakam740 <apnakam740@gmail.com>
2026-03-22 16:40:39 +0530

    Update vars.py

--- Modules/vars.py
+++ Modules/vars.py
@@ -5,7 +5,7 @@ from os import environ
 
 API_ID = int(environ.get("API_ID", "22182189"))
 API_HASH = environ.get("API_HASH", "5e7c4088f8e23d0ab61e29ae11960bf5")
-BOT_TOKEN = environ.get("BOT_TOKEN", "")
+BOT_TOKEN = environ.get("BOT_TOKEN", "8543256899:AAFHodLHKJsxJfWpmJCKKfBK-gE5gzGU_BY")
 
 OWNER = int(environ.get("OWNER", "6410036740"))
 CREDIT = environ.get("CREDIT", "VIПΛY")
